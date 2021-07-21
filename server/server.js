4e008ccc2176cdc17f005cb90dfcd66c35602c96
Skinbow <spyro.yakimenko@gmail.com>
2021-07-21 22:59:51 +0200

    Dynamically assigned port

--- server/server.js
+++ server/server.js
@@ -192,6 +192,6 @@ process.on("SIGINT", terminate);
 // Debug, signal sent by nodemon when restarting
 process.on("SIGUSR2", terminate);
 
-server.listen(5000, () => {
-    console.log("Listenting on *:5000");
+server.listen(process.env.PORT || 5000, () => {
+    console.log("Listenting on *:" + process.env.PORT.toString() || "5000");
 });
